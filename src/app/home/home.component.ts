657e83d4dafd9bc0f3cd601d01c5cc397a5ee910
Andres1650 <andresnegrete1650@gmail.com>
2019-07-03 16:44:19 -0700

    Updating

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive } from '@angular/core';
+
+
+@Directive({
+  selector: 'my-directive',
+})
+
+export class MyDirective {
+  
+}
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
