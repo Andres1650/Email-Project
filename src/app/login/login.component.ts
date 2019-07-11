405710ccd57245c168681ac16cbb0d3fbe100aad
Andres1650 <andresnegrete1650@gmail.com>
2019-07-11 16:38:16 -0700

    added login form (working on pop up functionality)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor() { }
 
-  ngOnInit() {
+  validateQuantity(): void{
+    let quantity = this.loginForm.value.quantity.toString();
+    quantity =quantity.replace(/D+/g, '');
+    this.loginForm.controls.quantity.setValue(+quantity);
   }
 
+  ngOnInit(){
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+    })
+    
+    this.loginForm.valueChanges.subscribe(
+      (value) => console.log(value)
+    );
+    this.loginForm.valueChanges.subscribe(
+      (status) => console.log(status)
+    ); 
+  }
+
+  
+  onSubmit(){
+    console.log(this.loginForm)
+    }
+  
+  onReset(){
+    this.loginForm.reset();
+    }
 }
